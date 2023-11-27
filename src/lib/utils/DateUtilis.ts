0670f8f482ebf47750ddb2e67059154804b9b247
G@nesh <ganeshparmarms7@gmail.com>
2023-11-27 21:08:05 +0530

    date friendly input

--- src/lib/utils/DateUtilis.ts
+++ src/lib/utils/DateUtilis.ts
@@ -1,6 +1,5 @@
 import type { DMY } from '$lib/types/DMY';
 import dayjs from 'dayjs';
-import TruckOutline from '../../../node_modules/flowbite-svelte-icons/dist/TruckOutline.svelte';
 // get Number of Months
 
 export const calculateDiffBetweenDates = (
@@ -26,7 +25,7 @@ export const calculateDiffBetweenDates = (
 export const calculateDiffInDMY = (startDate: Date, endDate: Date): DMY => {
 	const diffEndDate = dayjs(endDate);
 	const diffStartDate = dayjs(startDate);
-	const months : number= diffEndDate.diff(startDate, 'month');
+	const months: number = diffEndDate.diff(startDate, 'month');
 
 	// alert(diffEndDate.get('month')+1);
 
@@ -75,24 +74,66 @@ export const calculateDiffInDMY = (startDate: Date, endDate: Date): DMY => {
 	} else {
 		year = endDateYear;
 	}
-						
-	console.log('Previos year before endmonth ', year);	
+
+	console.log('Previos year before endmonth ', year);
 
 	console.log('Created date ', createDateWithDayjs(date, month, year));
 
-	return { months , days: diffEndDate.diff(createDateWithDayjs(date, month, year), 'day') };
+	return { months, days: diffEndDate.diff(createDateWithDayjs(date, month, year), 'day') };
 };
 
 const createDateWithDayjs = (date: number, month: number, year: number): dayjs => {
-	const d : Date= new Date(year, month, date)
-	const day : dayjs = dayjs(d)
+	const d: Date = new Date(year, month, date);
+	const day: dayjs = dayjs(d);
 	return day;
 };
 
-export function isValidateDates(endDate:Date,startDate:Date) {
-    if (endDate < startDate) {
-      return false;
-    } else {
-      return true;
-    }
-  }
+export function isValidateDates(endDate: Date, startDate: Date) {
+	if (endDate < startDate) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+export function formatDateToString(date: Date) {
+	// Format a JavaScript Date object as "MM-DD-YYYY"
+	const mm = String(date.getMonth() + 1).padStart(2, '0');
+	const dd = String(date.getDate()).padStart(2, '0');
+	const yyyy = date.getFullYear();
+	return `${dd}-${mm}-${yyyy}`;
+}
+
+export function formatDate(dateValue: string) {
+	// Remove any non-numeric characters
+	const sanitized = dateValue.replace(/[^0-9]/g, '');
+
+	// Use regular expressions to add dashes in the appropriate places
+	const formatted = sanitized.replace(/(\d{2})(\d{0,2})(\d{0,4})/, (_, p1, p2, p3) => {
+		let result = p1;
+		if (p2) result += `-${p2}`;
+		if (p3) result += `-${p3}`;
+		return result;
+	});
+
+	// Set the formatted value back to the input
+	dateValue = formatted;
+	return dateValue;
+}
+
+export function parseDate(dateValue: string) {
+	// Parse the input value and update the initialDate if the input is a valid date
+	const parts = dateValue.split('-');
+
+	// parts 2 -> YYYY
+	// parts 1 -> MM
+	// parts 0 -> DD
+
+	const parsedDate = new Date(`${parts[1]}-${parts[0]}-${parts[2]}`);
+
+	// if (!isNaN(parsedDate.getTime())) {
+	//   initialDate = parsedDate;
+	// }
+
+	return parsedDate;
+}
--- src/routes/monthcalculator/MyDate.svelte
+++ src/routes/monthcalculator/MyDate.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { formatDate, formatDateToString, parseDate } from '$lib/utils/DateUtilis';
 	import { A, Input } from 'flowbite-svelte';
 
 	export let date_: Date | null | string;
@@ -6,27 +7,37 @@
 	export let defaultToday : boolean;
 
 	let changedDate: Date | null;
-
+	let dateString : string;
 
 	const changeDateToCurrent = () => {
 		const currentDate = new Date();
 		// date_ = currentDate;
 		// Format the date in "yyyy-MM-dd" and set it as the input value
-		const formattedDate = currentDate.toISOString().split('T')[0];
-        date_ = formattedDate;
+		// const formattedDate = currentDate.toISOString().split('T')[0];
+		dateString = formatDateToString(currentDate);
+		date_ = parseDate(dateString);
 	};
 
 	if(defaultToday){
 		changeDateToCurrent()
 	}
+
+	function handleOnInput(){
+		dateString = formatDate(dateString);
+		date_ = parseDate(dateString);
+		console.log(date_);
+	}
+
 </script>
 
 <span class="lg:inline-block md:m-auto m-2  lg:w-96">
 	<Input
-		datepicker
 		type="text"
-		bind:value={date_}
+		bind:value={dateString}
 		required
+		on:input={handleOnInput}
+		placeholder="dd-mm-yyyy"
+		maxLength={10}
 	/>
 	
 	<A on:click={changeDateToCurrent}>( Now )</A>
